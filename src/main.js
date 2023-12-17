1d5425c2b2579d0b30e138938e51acfed3c17a36
Ivan Ljubicic <ivanljubicic36@gmail.com>
2023-12-17 21:17:17 +0100

    pokusaj fixanja notifikacija

--- src/main.js
+++ src/main.js
@@ -1,37 +1,29 @@
 import App from './App.svelte';
 
 const app = new App({
-	target: document.body,
+  target: document.body,
 });
 
-if ('serviceWorker' in navigator) {
-	navigator.serviceWorker.register('/service-worker.js')
-	  .then(registration => {
-		console.log('Service Worker registered with scope:', registration.scope);
-		return registration;
-	  })
-	  .then(registration => {
-		// Wait until the service worker is active
-		return navigator.serviceWorker.ready;
-	  })
-	  .then(registration => {
-		// Now it's safe to show notifications
-		return registration.showNotification('Sucess');
-	  })
-	  .catch(error => {
-		console.error('Service Worker registration failed:', error);
-	  });
-  }
-  
-  
-  if ('Notification' in window) {
-	Notification.requestPermission().then(permission => {
-	  if (permission === "granted") {
-		console.log("Notification permission granted.");
-		// You can now use new Notification() to show notifications
-	  }
-	});
-  }
-  
+// First, request permission for notifications
+if ('Notification' in window) {
+  Notification.requestPermission().then(permission => {
+    if (permission === "granted") {
+      // Once permission is granted, register the service worker
+      if ('serviceWorker' in navigator) {
+        navigator.serviceWorker.register('/service-worker.js')
+          .then(registration => {
+            console.log('Service Worker registered with scope:', registration.scope);
+            // Show the notification through the service worker
+            registration.showNotification("Notifications enabled!");
+          })
+          .catch(error => {
+            console.error('Service Worker registration failed:', error);
+          });
+      }
+    } else {
+      console.error('Notification permission not granted:', permission);
+    }
+  });
+}
 
 export default app;
